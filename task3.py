5c1a836343fd362cbf1c5b58f026c7155f16442f
greenfest <1greenfest@gmail.com>
2022-10-09 20:22:26 +0500

    added task3

---
+++ task3.py
@@ -0,0 +1,7 @@
+num = int(input("Введите номер четверти: "))
+
+if   num == 1: print("x from 0 to + ∞, y from 0 to + ∞") 
+elif num == 2: print("x from 0 to  - ∞, y from 0  to + ∞ ")  
+elif num == 3: print("x from 0 to  - ∞, y from 0  to - ∞ ")
+elif num == 4: print("x from 0 to  + ∞, y from 0  to - ∞ ")
+else:          print("Ошибка, указан некорректный номер четверти") 
